faa211e7f0a889fe7f8bc9aa9aacf7d1b380e02e
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-02-08 11:15:43 +0530

    router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Userdetail from "./Userdetail";
+import Users from "./Users";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Router>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Userdetail users={users} />} />
+            <Route path="/users/:id" element={<Users users={users} />} />
+          </Routes>
+        )}
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
